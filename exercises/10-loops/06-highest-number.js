dc2a5eb4b626f6e2c10be6e875fdde91dd4570eb
matinaspatsos <matinaspatsos@gmail.com>
2024-10-13 11:52:26 -0400

    Copy update to highest number exercise

--- exercises/10-loops/06-highest-number.js
+++ exercises/10-loops/06-highest-number.js
@@ -2,7 +2,7 @@ let numbers = [10000, -8, 10, 0.7]; // e.g.
 
 /**
  * Create a variable called "highest".
- * Loop through the array using a for loop (or for ... of loop) and return the highest number.
+ * Loop through the array using a for loop (or for ... of loop) and find the highest number.
  * Set "highest" to the highest number.
  */
 
